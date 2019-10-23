88985a2ce2aee446aa7434ecf553d2948e07fbef
Unknown <alyssasgable@gmail.com>
2019-10-23 14:26:23 -0500

    added navbar to home

--- adopt-a-classroom/src/views/Home.vue
+++ adopt-a-classroom/src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="home">
+     <b-navbar toggleable="sm" type="light" variant="light">
+     <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+     <b-navbar-brand>Adopt A Classroom</b-navbar-brand>
+
+     <b-collapse id="nav-text-collapse" is-nav>
+       <b-navbar-nav>
+          <b-nav-item><router-link to="home">Home</router-link></b-nav-item>
+          <b-nav-item><router-link to="schools">Schools</router-link></b-nav-item>
+          <b-nav-item><router-link to="profile">My Profile</router-link></b-nav-item>
+
+          <b-button @click="logout" variant="danger">Logout</b-button><br><br>
+       </b-navbar-nav>
+     </b-collapse>
+   </b-navbar>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Adopt A Classroom</h1>
     <div id="nav">
-      
-      <b-button @click="logout" variant="danger">Logout</b-button><br><br>
     </div>
   </div>
 </template>
